cb7701c248da2b25f92d64d7c3050e267ef16cff
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-06-12 11:50:15 -0700

    Implementing wave 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,49 @@
 import TaskList from './components/TaskList.jsx';
 import './App.css';
-
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+import { useState } from 'react';
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: 'Mow the lawn',
+      isComplete: false,
+    },
+    {
+      id: 2,
+      title: 'Cook Pasta',
+      isComplete: true,
+    },
+  ]);
+
+  const toggleTaskComplete = (id) => {
+    const updatedTask = tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, isComplete: !task.isComplete };
+      }
+      return task;
+    });
+    setTasks(updatedTask);
+  };
+
+  const deleteTask = (id) => {
+    const updatedTasks = tasks.filter((task) => {
+      task.id !== id;
+    });
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>{<TaskList
+          tasks={tasks}
+          onToggleComplete={toggleTaskComplete}
+          onDeleteTask={deleteTask}
+        />}</div>
       </main>
     </div>
   );
